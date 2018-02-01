86033d01308e2145ddd6199e4143699c3aa93c13
Henrique <Henrique@Henrique-PC>
2018-01-31 21:17:46 -0300

    Lucas commit

--- src/br/com/ifpe/uevents/Controller/UsuarioController.java
+++ src/br/com/ifpe/uevents/Controller/UsuarioController.java
@@ -1,8 +1,26 @@
 package br.com.ifpe.uevents.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.ifpe.uevents.Dao.UsuarioDao;
+import br.com.ifpe.uevents.Model.Usuario;
 
 @Controller
 public class UsuarioController {
-
+	
+	@RequestMapping("exibirLogin")
+        public String exibirLogin(){ 
+		return "index.jsp";
+	}
+	@RequestMapping("efetuaLogin")
+	public String efetuaLogin(Usuario usuario, HttpSession session) {
+	if(new UsuarioDao().login(usuario)) {
+	session.setAttribute("usuarioLogado", usuario);
+	return "index.jsp";
+	}
+	return "redirect:";
+	}
 }
