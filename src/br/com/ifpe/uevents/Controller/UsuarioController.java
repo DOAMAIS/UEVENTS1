5fbb8afa0453303038b03991f54d4b09bc4421f4
alunos ifpe <doamais_ifpe@outlook.com>
2018-02-22 20:10:24 -0300

    Ed - 22/02

--- src/br/com/ifpe/uevents/Controller/UsuarioController.java
+++ src/br/com/ifpe/uevents/Controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.uevents.Controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -7,28 +9,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.ifpe.uevents.Dao.UsuarioDao;
+import br.com.ifpe.uevents.Model.Atividade;
 import br.com.ifpe.uevents.Model.Usuario;
 
 @Controller
 public class UsuarioController {
 	
-	@RequestMapping("exibirLogin")
-        public String exibirLogin(){ 
-		return "index.jsp";
+	@RequestMapping("/cadasUsuario")
+	public String cadastro(){
+		return "telas/cadasUsuario";
+	}
+	@RequestMapping("inserirUsuario")
+	public String inserirUser(Usuario usuario){
+		UsuarioDao dao = new UsuarioDao();
+		dao.cadastrar(usuario);
+		return "telas/cadasUsuario";
 	}
 	@RequestMapping("efetuaLogin")
 	public String efetuaLogin(Usuario usuario, HttpSession session, Model model) {
 		UsuarioDao dao = new UsuarioDao();
 		Usuario usuarioLogado = dao.buscarUsuario(usuario);
 		if (usuarioLogado != null) {
+			//usuarioLogado.setAtividades(dao.listarAtvs(usuario));
+			List<Atividade> atvsUsuarioLogado = new UsuarioDao().listarAtvs(usuarioLogado);
+			model.addAttribute("atvsUsuarioLogado", atvsUsuarioLogado);
 			session.setAttribute("usuarioLogado", usuarioLogado);
-	   return "telas/inicialEvento";
+		    return "telas/inicialEvento";
+		}
+		model.addAttribute("msg", "Login e/ou senha inválidos.");
+		return "telas/index";
 	}
-	   model.addAttribute("msg", "Não foi encontrado um usuário com o login e senha informados.");
-			   return "index";
-	}
-	@RequestMapping("exibirCadastro")
-		public String exibirCadastro(){
-		return "telas/cadasUsuario";
-	}
+
 }
