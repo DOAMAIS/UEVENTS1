ab2e30ff126d5ef63d88573dc47c930e4a0aa597
alunos ifpe <doamais_ifpe@outlook.com>
2018-03-15 16:38:01 -0300

    Ed - 15/03

--- src/br/com/ifpe/uevents/Controller/UsuarioController.java
+++ src/br/com/ifpe/uevents/Controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package br.com.ifpe.uevents.Controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -8,13 +9,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itextpdf.text.DocumentException;
+
 import br.com.ifpe.uevents.Dao.AtividadeDao;
 import br.com.ifpe.uevents.Dao.EventoDao;
 import br.com.ifpe.uevents.Dao.UsuarioDao;
 import br.com.ifpe.uevents.Model.Atividade;
 import br.com.ifpe.uevents.Model.Evento;
 import br.com.ifpe.uevents.Model.Usuario;
+import br.com.ifpe.uevents.util.HtmlToPdf;
 import br.com.ifpe.uevents.util.Mensagens;
+import br.com.ifpe.uevents.util.Util;
 
 @Controller
 public class UsuarioController {
@@ -76,7 +81,7 @@ public class UsuarioController {
 				session.setAttribute("usuarioLogado", usuarioLogado);
 			    return "forward:paginaInicial";
 			}
-			model.addAttribute("msg", Mensagens.UsuarioSenhaInvalido);
+			model.addAttribute("msg", "Login e/ou senha inválidos.");
 			return "telas/index";
 		}
 
@@ -131,33 +136,25 @@ public class UsuarioController {
 		}*/
 
 		@RequestMapping("/exibirAlterarUsuario")
-	    public String exibirAlterarUsuario(Usuario Usuario, Model model,HttpSession session) {
-		UsuarioDao dao = new UsuarioDao();
-		Usuario usu = (Usuario) session.getAttribute("usuarioLogado");
-		model.addAttribute("usuario", usu);
-		return "telas/alterarUsuario";
+	    public String exibirAlterarUsuario() {
+			return "telas/alterarUsuario";
 	    }
 		@RequestMapping("/alterarUsuario")
 	    public String alterarUsuario(Usuario Usuario, Model model) {	
-		UsuarioDao dao = new UsuarioDao();
-		dao.alterar(Usuario);
-		model.addAttribute("msg", "Usuario Alterado com Sucesso!");
-		return "forward:home";
+			UsuarioDao dao = new UsuarioDao();
+			dao.alterar(Usuario);
+			model.addAttribute("msg", Mensagens.UsuarioAterado);
+			return "forward:paginaInicial";
 	    }
-	@RequestMapping("voltaHome")
-	       public String voltaHome(HttpSession session, Model model){	
-			Usuario usuarioLogado = (Usuario) session.getAttribute("usuarioLogado");
-			//Lista de Eventos
-			List<Evento> listaEventos = new EventoDao().listar();
-			model.addAttribute("listaEventos", listaEventos);
-			//Lista de Atividades
-			List<Atividade> listaAtividades = new AtividadeDao().listarAtividadeUsuario(usuarioLogado);
-			model.addAttribute("listaAtividades", listaAtividades);
-			//Lista de Atividades do UsuÃ¡rio
-			List<Atividade> atvsUsuarioLogado = new UsuarioDao().listarAtvs(usuarioLogado);
-			model.addAttribute("atvsUsuarioLogado", atvsUsuarioLogado);
-			return "telas/inicialEvento";
-}
+		
+		@RequestMapping("/gerarAta")
+	    public String gerarAta(Atividade atv, Model model) throws IOException, DocumentException {	
+			String nomeAtv = new AtividadeDao().buscarPorId(atv).getNome();
+			HtmlToPdf.createPdf(Util.geraSalt()+"ata_da_atv_"+nomeAtv, atv);
+			
+			model.addAttribute("msg", "Ata gerada com sucesso");
+			return "forward:paginaInicial";
+	    }
 		
 		 @RequestMapping("/logout")
 		 public String logout(HttpSession session) {
